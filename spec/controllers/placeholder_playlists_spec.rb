4a61063cdd06c77a2955b90d1523b859bc4220f1
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-26 10:47:58 +0100

    Adds additional output to /placeholder/playlists

--- spec/controllers/placeholder_playlists_spec.rb
+++ spec/controllers/placeholder_playlists_spec.rb
@@ -17,8 +17,17 @@ RSpec.describe PlaceholderPlaylistsController, type: :controller do
             "id": "1",
             "title": "playlist1",
             "uuid": "40c5b468-13af-483d-8728-eb4f85a9f765"
+          },
+          {
+            "id": "2",
+            "title": "playlist2",
+            "uuid": "ddfd3bcc-bc63-4d78-88e6-8bcc17c0bd41"
+          },
+          {
+            "id": "3",
+            "title": "playlist3",
+            "uuid": "0ccba4e9-1a7f-43f0-8672-ce8c7eaa7361"
           }
-      
         ]
       )
     end
