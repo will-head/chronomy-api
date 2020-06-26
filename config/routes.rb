33b827533e616636de97e85ca43065d1f6e69b75
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-26 17:24:42 +0100

    Adds User model and Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "static#home"
 
+  resources :sessions, only: [:create]
+
   get "/placeholder/playlist/40c5b468-13af-483d-8728-eb4f85a9f765" => "placeholder_playlist#home"
   get "/placeholder/playlists/" => "placeholder_playlists#home"
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.create!(email: "example@example.com", username: "example", password: "password") }
+  it { is_expected.to be }
+  
 end
