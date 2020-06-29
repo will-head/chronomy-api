74b07f019db95266706066b5f9fe19296349c9a0
mbrad26 <mbrad26@gmail.com>
2020-06-29 08:39:20 +0100

    Added test for when user has no password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,10 +15,20 @@ RSpec.describe User, type: :model do
   context 'when incorect attributes' do
     it 'User is not valid with no username' do
       user = User.create(username: nil, email: 'email@example.com', password: 'password')
+
+      expect(user).to_not be_valid
     end
 
     it 'User is not valid with no email' do
       user = User.create(username: 'example20', email: nil, password: 'password')
+
+      expect(user).to_not be_valid
+    end
+
+    it 'User is not valid with no password' do
+      user = User.create(username: 'example20', email: 'email@example.com', password: nil)
+
+      expect(user).to_not be_valid
     end
   end
 end
