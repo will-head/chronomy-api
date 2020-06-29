781fa81b55877ec097aec7c013016cce082a8658
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-29 11:54:37 +0100

    Changes cors.rb to allow :3001

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
-    allow do
+  allow do
+    origins "http://localhost:3001"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
     origins "http://localhost:5000"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
