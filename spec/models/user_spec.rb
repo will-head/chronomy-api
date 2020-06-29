af0cb5cd077f3ae2d68a0a8a6a45b883191bad5c
mbrad26 <mbrad26@gmail.com>
2020-06-29 08:33:10 +0100

    Added test for when user creation attributes are correct

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject(:user) { User.create!(email: "example@example.com", username: "example", password: "password") }
   it { is_expected.to be }
-  
+
+  context 'when correct attributes' do
+    it 'User is valid' do
+      user = User.create(username: 'example20', email: 'email@example.com', password: 'password')
+
+      expect(user).to be_valid
+    end
+  end
+
 end
