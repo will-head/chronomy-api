a3986809fefbc055afaf5d8711d59d3cedb5bb7f
mbrad26 <mbrad26@gmail.com>
2020-06-30 17:42:39 +0100

    Before pull request

--- spec/controllers/playlists_controller_spec.rb
+++ spec/controllers/playlists_controller_spec.rb
@@ -17,13 +17,6 @@ RSpec.describe PlaylistsController, type: :controller do
 
         json = JSON.parse(response.body)
 
-        # p response.body
-        # p json['playlist']
-        # p request['playlist']
-        # p Playlist.first
-        # p Playlist.first
-        # p Playlist.all
-
         # expect(json["status"]).to eq 200
         expect(Playlist.last).to eq playlist
         # expect(playlist.user_id).to eq user.id
@@ -32,17 +25,33 @@ RSpec.describe PlaylistsController, type: :controller do
     end
   end
 
+  describe '#show' do
+    context 'when the playlist exists' do
+      it 'renders status 200' do
+        get :show, params: { id: playlist.id }, format: :json
+        json = JSON.parse(response.body)
 
-    describe '#show' do
-      context 'when the playlist exists' do
-        it 'renders status 200' do
-          get :show, params: { id: playlist.id }, format: :json
-          json = JSON.parse(response.body)
-
-          expect(json['status']).to eq 200
-          expect(json['playlist']['id']).to eq playlist.id
-          expect(json['playlist']['uuid']).to eq playlist.uuid
-        end
+        expect(json['status']).to eq 200
+        expect(json['playlist']['id']).to eq playlist.id
+        expect(json['playlist']['uuid']).to eq playlist.uuid
       end
     end
+
+    context 'when playlist does not exists' do
+      it 'renders status 500' do
+        # allow(subject).to receive(:show) { 500 }
+        #
+        # # playlist = double ":playlist", :id
+        # # playlist = Playlist.new(title: '', user_id: user.id, uuid: SecureRandom.uuid)
+        # # Playlist.create(title: nil, user_id: user.id, uuid: nil)
+        # # not_found = Playlist.find_by(title: 'Hello World')
+        # # get :show, params: { id: playlist.id }, format: :json
+        # json = JSON.parse(response.body)
+        # # p response.body
+        # p json
+        #
+        # expect(json['status']).to eq ""
+      end
+    end
+  end
 end
