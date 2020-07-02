39659e0c718ccaf6d344865ae93ee4e12deaf42b
@willhead <60652390+will-head@users.noreply.github.com>
2020-07-02 10:01:21 +0100

    Adds chronomy.net to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,4 +18,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "http://chronomy.surge.sh"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+
+  allow do
+    origins "http://chronomy.net"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
